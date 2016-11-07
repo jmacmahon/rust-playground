0cb08bbb5947e8f70f86976b9a55702605ddbba2
Joe MacMahon <joe@macmahon.eu>
2016-11-07 15:41:51 +0000

    playing with vectors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut v: Vec<i32> = vec![21, 34, 55, 89];
+
+    loop {
+        println!("{:?}", v);
+        // let index = match read_and_parse() {
+        //     Ok(num) => num,
+        //     Err(_) => break,
+        // };
+        let index: i32 = read_and_parse().expect("uh oh");
+
+        v.push(index);
+    }
+}
+
+fn read_and_parse<T>() -> Result<T, T::Err> where T: std::str::FromStr {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).expect("Failed to read line");
+
+    buf.trim().parse()
 }
